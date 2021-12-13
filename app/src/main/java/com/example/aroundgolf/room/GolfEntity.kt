b497ba493f4b10716e9da9c0d1e97cd9d663134d
Parkduksung <duksung1234@naver.com>
2021-12-13 22:30:30 +0900

    [feature] golf roomdb 추가.

--- app/src/main/java/com/example/aroundgolf/room/GolfEntity.kt
+++ app/src/main/java/com/example/aroundgolf/room/GolfEntity.kt
@@ -4,14 +4,13 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "camping_table")
-data class CampingEntity(
+@Entity(tableName = "golf_table")
+data class GolfEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo(name = "contentId") val contentId: String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "address") val address: String,
-    @ColumnInfo(name = "homepage") val homepage: String? = null,
     @ColumnInfo(name = "tel") val tel: String? = null,
     @ColumnInfo(name = "latitude") val lat: Double,
-    @ColumnInfo(name = "longitude") val log: Double
+    @ColumnInfo(name = "longitude") val log: Double,
+    @ColumnInfo(name = "like") val like: Boolean,
 )
