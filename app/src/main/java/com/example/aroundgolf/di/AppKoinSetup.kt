6deae8c0353528530206eade2f20d1393ab42ba3
Parkduksung <duksung1234@naver.com>
2021-12-13 22:33:06 +0900

    [feature] di 추가.

--- app/src/main/java/com/example/aroundgolf/di/AppKoinSetup.kt
+++ app/src/main/java/com/example/aroundgolf/di/AppKoinSetup.kt
@@ -1,5 +1,14 @@
 package com.example.aroundgolf.di
 
+import androidx.room.Room
+import com.example.aroundgolf.api.GolfApi
+import com.example.aroundgolf.data.repo.GolfRepository
+import com.example.aroundgolf.data.repo.GolfRepositoryImpl
+import com.example.aroundgolf.data.source.local.GolfLocalDataSource
+import com.example.aroundgolf.data.source.local.GolfLocalDataSourceImpl
+import com.example.aroundgolf.data.source.remote.GolfRemoteDataSource
+import com.example.aroundgolf.data.source.remote.GolfRemoteDataSourceImpl
+import com.example.aroundgolf.room.GolfDatabase
 import com.example.aroundgolf.viewmodel.BookmarkViewModel
 import com.example.aroundgolf.viewmodel.HomeViewModel
 import com.example.aroundgolf.viewmodel.MapViewModel
@@ -8,12 +17,14 @@ import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AppKoinSetup : KoinBaseKoinSetup() {
 
     companion object {
-        private const val GO_CAMPING_BASE_URL =
-            "http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/"
+        private const val GOLF_BASE_URL =
+            "https://openapi.gg.go.kr/"
     }
 
     private val viewModelModule = module {
@@ -23,52 +34,43 @@ class AppKoinSetup : KoinBaseKoinSetup() {
         viewModel { SplashViewModel(androidApplication()) }
     }
 
-//    private val repositoryModule = module {
-//        single<GoCampingRepository> { GoCampingRepositoryImpl() }
-//        single<FirebaseRepository> { FirebaseRepositoryImpl() }
-//    }
-//
-//    private val sourceModule = module {
-//        single<GoCampingRemoteDataSource> { GoCampingRemoteDataSourceImpl() }
-//        single<GoCampingLocalDataSource> { GoCampingLocalDataSourceImpl() }
-//        single<FirebaseRemoteDataSource> {
-//            FirebaseRemoteDataSourceImpl(
-//                FirebaseAuth.getInstance(),
-//                FirebaseFirestore.getInstance(),
-//                FirebaseStorage.getInstance()
-//            )
-//        }
-//    }
-//
-//    private val apiModule = module {
-//        single<GoCampingApi> {
-//            Retrofit.Builder()
-//                .baseUrl(GO_CAMPING_BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build()
-//                .create(GoCampingApi::class.java)
-//        }
-//    }
-//
-//    private val databaseModule = module {
-//        single {
-//            Room.databaseBuilder(
-//                get(),
-//                CampingDatabase::class.java,
-//                "camping_database"
-//            )
-//                .fallbackToDestructiveMigration()
-//                .build()
-//        }
-//    }
+    private val repositoryModule = module {
+        single<GolfRepository> { GolfRepositoryImpl() }
+    }
+
+    private val sourceModule = module {
+        single<GolfRemoteDataSource> { GolfRemoteDataSourceImpl() }
+        single<GolfLocalDataSource> { GolfLocalDataSourceImpl() }
+    }
+    private val apiModule = module {
+        single<GolfApi> {
+            Retrofit.Builder()
+                .baseUrl(GOLF_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GolfApi::class.java)
+        }
+    }
+
+    private val databaseModule = module {
+        single {
+            Room.databaseBuilder(
+                get(),
+                GolfDatabase::class.java,
+                "golf_database"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+    }
 
     override fun getModules(): List<Module> {
         return listOf(
-            viewModelModule
-//            repositoryModule,
-//            sourceModule,
-//            apiModule,
-//            databaseModule
+            viewModelModule,
+            repositoryModule,
+            sourceModule,
+            apiModule,
+            databaseModule
         )
     }
 }
